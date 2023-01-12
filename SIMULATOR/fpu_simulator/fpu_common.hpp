9b366d7b00bc74b4479928c6e7118caf0ad97015
mm1129 <89204774+mm1129@users.noreply.github.com>
2023-01-12 14:04:46 +0900

    Update fpu_common.hpp

--- SIMULATOR/fpu_simulator/fpu_common.hpp
+++ SIMULATOR/fpu_simulator/fpu_common.hpp
@@ -2,8 +2,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define Bit32 unsigned int
-unsigned long sub_uint (unsigned long i, int a, int b);
-unsigned long at_uint (unsigned long i, int a);
+#define Bit64 unsigned long long
+extern vector<Bit64> array0;
+Bit64 sub_uint (Bit64 i, int a, int b);
+Bit64 at_uint (Bit64 i, int a);
 unsigned int f_to_bit (float x);
 void print_bit(Bit32 x, int n);
 void print_sub_bit(Bit32 y);
+void in_data();
